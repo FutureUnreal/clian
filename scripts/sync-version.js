5698fefe8f3ea10dfd7409c15074fe7bd2321f1f
FutureUnreal <42101210307@stu.xpu.edu.cn>
2026-03-07 01:37:33 +0800

    chore: automate versions.json sync for releases

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -5,12 +5,25 @@ const path = require('path');
 
 const packagePath = path.join(__dirname, '..', 'package.json');
 const manifestPath = path.join(__dirname, '..', 'manifest.json');
+const versionsPath = path.join(__dirname, '..', 'versions.json');
 
 const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 const manifestJson = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+const versionsJson = fs.existsSync(versionsPath)
+  ? JSON.parse(fs.readFileSync(versionsPath, 'utf8'))
+  : {};
 
 manifestJson.version = packageJson.version;
 
-fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2) + '\n');
+if (!manifestJson.minAppVersion) {
+  throw new Error('manifest.json is missing minAppVersion');
+}
 
-console.log(`Synced version to ${packageJson.version}`);
+versionsJson[packageJson.version] = manifestJson.minAppVersion;
+
+fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2) + '\n');
+fs.writeFileSync(versionsPath, JSON.stringify(versionsJson, null, 2) + '\n');
+
+console.log(
+  `Synced ${packageJson.version} to manifest.json and versions.json (${manifestJson.minAppVersion})`
+);
